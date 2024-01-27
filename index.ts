68c603e489032b0389767d1422edde02d863d816
nurbekbekbek <salayevnurbek0@gmail.com>
2024-01-27 15:47:37 +0500

    bot

--- index.ts
+++ index.ts
@@ -1,8 +1,35 @@
 import env from "./config/env.config.js";
-import { Bot, InlineKeyboard, Keyboard } from "grammy";
+import { Bot, Context, GrammyError, HttpError, Keyboard } from "grammy";
 import { sequentialize, run } from "@grammyjs/runner";
+// interface BotSetings {
+//   userName: string | undefined;
+// }
 
+// type MyContext = Context & {
+//   settings: BotSetings;
+// }
 const bot = new Bot(env.TOKEN);
+
+// bot.use(async (ctx, next) => {
+//   ctx.settings = {
+//     userName: "alisher"
+//   }
+//   await next()
+// })
+
+async function setCommands() {
+  await bot.api.setMyCommands([
+    {
+      command: "start",
+      description: `Добро пожаловать в наш бот 😊`
+    },
+    {
+      command: "location",
+      description: `где мы находимся 🚩`
+    },
+  ])
+}
+setCommands()
 
 bot.use(
   sequentialize((ctx: any) => {
@@ -12,10 +39,18 @@ bot.use(
   })
 );
 
-const auth = {
-  signup: false,
-  login: false,
-};
+bot.command("location", (ctx) => {
+  ctx.api.sendLocation(ctx.chat.id, 41.560067458778754, 60.60795898367056)
+})
+
+bot.command("start", (ctx) => {
+  ctx.reply("Добро пожаловать в наш бот с чем могу помочь 😃", {
+    reply_markup: new Keyboard()
+    .text("products")
+    .text("category")
+    .resized()
+  })
+})
 
 bot.on("message", async (ctx: any) => {
   try {
@@ -24,64 +59,26 @@ bot.on("message", async (ctx: any) => {
     const messageId = ctx.message?.message_id;
 
     if (chatId && messageId) {
-        await ctx.api.deleteMessage(chatId, messageId);
+      await ctx.api.deleteMessage(chatId, messageId);
     }
 
-    if (message.startsWith("/start")) {
-      ctx.reply(`Добро пожаловать в наш бот ${ctx.from.first_name} 😊`, {
-        reply_markup: new InlineKeyboard()
-          .text("login", "login")
-          .text("signup", "signup"),
-      });
-      return;
-    }
-    // if (message.startsWith("/start")) {
-    //   await ctx.reply(`Добро пожаловать в наш бот ${ctx.from.first_name} 😊`, {
-    //     reply_markup: new Keyboard().requestContact("Telefon raqam"),
-    //   });
-    // }
-
-    if (auth.signup) {
-        ctx.reply('signup')
-        auth.signup=false
-        return
-    }
-
-    if (auth.login) {
-        ctx.reply('login')
-        auth.login=false
-        return
-    }
-
-    // const newKeyboard = new Keyboard().requestContact("Telefon raqam");
-
-    await ctx.api.sendMessage(chatId, "<a href='http://localhost/phpMyAdmin/index.php?route=/sql&server=1&db=telegramshop&table=user&pos=0' >я пока не могу поддержать диалог 😑</a>", {parse_mode: "HTML"})
+    ctx.reply("я пока не могу поддержать диалог 😑")
   } catch (error) {
     console.log(error);
   }
 });
 
-bot.on("callback_query:data", async (ctx) => {
-  await ctx.answerCallbackQuery();
-  try {
-    if (ctx.callbackQuery.data === "signup") {
-      ctx.reply("пишите пароль для регистрации пример");
-      auth.signup = true;
-      auth.login = false;
-      return;
-    }
-    if (ctx.callbackQuery.data === "login") {
-      ctx.reply("пишите пароль для входа пример");
-      auth.login = true;
-      auth.signup = false;
-      return;
-    }
-
-    const error = new Error("comand not found");
-    throw error;
-  } catch (error) {
-    ctx.reply("ошибка при команде");
+bot.catch((err) => {
+  const ctx = err.ctx
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
   }
-});
+})
 
 run(bot);
